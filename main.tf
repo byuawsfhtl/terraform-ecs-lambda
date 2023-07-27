6a4d610febd97a09d6996cc0a5312e4daaf79c8c
hcorry98 <102935249+hcorry98@users.noreply.github.com>
2023-07-27 17:28:04 -0600

    Abstracted from ecs-lambda-template

---
+++ main.tf
@@ -0,0 +1,70 @@
+locals {
+  domain  = (var.env == "prd") ? "rll.byu.edu" : "rll-dev.byu.edu"
+  url     = lower("${var.project_name}.${local.domain}")
+  api_url = "api.${local.url}"
+}
+
+data "aws_ecr_repository" "ecr_repo" {
+  name = var.ecr_repo_name
+}
+
+module "acs" {
+  source = "github.com/byu-oit/terraform-aws-acs-info?ref=v4.0.0"
+}
+
+# ========== ECS ==========
+module "ecs_fargate" {
+  source = "github.com/byuawsfhtl/terraform-ecs-fargate"
+
+  app_name = var.project_name
+  primary_container_definition = {
+    name                  = "${var.project_name}Container"
+    image                 = "${data.aws_ecr_repository.ecr_repo.repository_url}:${var.app_name}-ecs-${var.image_tag}"
+    command               = var.ecs_command
+    environment_variables = {}
+    secrets               = {}
+  }
+  event_role_arn                = module.acs.power_builder_role.arn
+  vpc_id                        = module.acs.vpc.id
+  private_subnet_ids            = module.acs.private_subnet_ids
+  role_permissions_boundary_arn = module.acs.role_permissions_boundary.arn
+
+  task_policies = var.ecs_policies
+}
+
+# ========== API ==========
+module "lambda_api" {
+  source = "github.com/byuawsfhtl/terraform-lambda-api"
+
+  project_name  = var.project_name
+  app_name      = var.app_name
+  domain        = local.domain
+  url           = local.url
+  api_url       = local.api_url
+  ecr_repo_name = var.ecr_repo_name
+  image_tag     = "lambda-${var.image_tag}"
+  lambda_function_definitions = var.lambda_function_definitions
+  function_policies = concat(var.lambda_policies, [aws_iam_policy.ecs_policy])
+}
+
+# ========== IAM Policies ==========
+resource "aws_iam_policy" "ecs_policy" {
+  name        = "${var.project_name}-ecs"
+  description = "Permission to run the ${var.project_name} ecs task"
+
+  policy = jsonencode({
+    Version : "2012-10-17",
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : [
+          "ecs:RunTask"
+        ],
+        Resource : [
+          "${module.ecs_fargate.task_definition.arn}"
+        ]
+
+      }
+    ]
+  })
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "project_name" {
+    type = string
+    description = "The name of the project in TitleCase."
+}
+variable "app_name" {
+    type = string
+    description = "The name of the project in kebab-case."
+}
+
+variable "env" {
+    type = string
+    description = "The branch being deployed."
+}
+
+variable "ecr_repo_name" {
+    type = string
+    description = "The name of the ECR repository that contains the image for the lambda functions."
+}
+variable "image_tag" {
+    type = string
+    description = "The image tag for the Docker images (the timestamp)."
+}
+
+variable "ecs_command" {
+    type = list(string)
+    description = "The entry point for the Docker image used on the ECS task."
+    default = []
+}
+variable "ecs_policies" {
+    type        = list(string)
+  description = "List of IAM Policy ARNs to attach to the ECS task."
+  default     = []
+}
+
+variable "lambda_function_definitions" {
+  type = list(object({
+    path_part       = string
+    http_method     = string
+    command         = list(string)
+    allowed_headers = optional(string)
+    timeout         = optional(number)
+  }))
+  description = "The definitions for each lambda function."
+}
+variable "lambda_policies" {
+  type        = list(string)
+  description = "List of IAM Policy ARNs to attach to the task execution policy."
+  default     = []
+}
