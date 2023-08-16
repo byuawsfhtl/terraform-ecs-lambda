32adfe4676ce8cdf029622b110e0c73381d61631
hcorry98 <102935249+hcorry98@users.noreply.github.com>
2023-08-15 21:49:19 -0600

    Allow describe security groups

--- main.tf
+++ main.tf
@@ -60,7 +60,8 @@ resource "aws_iam_policy" "ecs_policy" {
         Effect : "Allow",
         Action : [
           "ecs:ListTasks",
-          "ecs:ListTaskDefinitions"
+          "ecs:ListTaskDefinitions",
+          "ec2:DescribeSecurityGroups"
         ],
         Resource : "*"
       }
