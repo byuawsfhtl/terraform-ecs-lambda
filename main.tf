abe03b3a3cab6da06185d39f88301e9c7994df47
jnienhui <jnienhuis132@gmail.com>
2024-10-30 11:45:22 -0600

    change lambda_api branch

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ module "ecs_fargate" {
 
 # ========== API ==========
 module "lambda_api" {
-  source = "github.com/byuawsfhtl/terraform-lambda-api?ref=prd"
+  source = "github.com/byuawsfhtl/terraform-lambda-api?ref=output-fixes"
 
   project_name                 = var.project_name
   app_name                     = var.app_name
